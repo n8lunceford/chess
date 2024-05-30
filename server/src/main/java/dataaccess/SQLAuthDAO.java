3cda5be85786599968202e2fae66e8f61f10fc7a
nathan <nathanlunceford@gmail.com>
2024-05-30 15:28:33 -0600

    Added supplemental methods to maintain consistent functionality for SQLAuthDAO.java as compared with MemoryAuthDAO.java

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,13 +1,54 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class SQLAuthDAO implements AuthDAO {
+
+    public SQLAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public ArrayList<AuthData> getAuthList() throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection();
+             var preparedStatement = connection.prepareStatement("SELECT * FROM auth");
+             var resultSet = preparedStatement.executeQuery()) {
+
+            ArrayList<AuthData> authList = new ArrayList<>();
+            //  Code where authList takes in values of preparedStatement
+            while (resultSet.next()) {
+                authList.add(new AuthData(resultSet.getString("authToken"), resultSet.getString("username")));
+            }
+            return authList;
+        }
+        catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
+
+    public int size() throws DataAccessException {
+        try (var connection = DatabaseManager.getConnection();
+             var preparedStatement = connection.prepareStatement("SELECT * FROM auth");
+             var resultSet = preparedStatement.executeQuery()) {
+
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+            else {
+                return 0;
+            }
+        }
+        catch (SQLException exception) {
+            throw new DataAccessException(exception.getMessage());
+        }
+    }
+
     @Override
     public void clear() throws DataAccessException {
-        try (var connection = DatabaseManager.getConnection(); var preparedStatement = connection.prepareStatement("DELETE user")) {
+        try (var connection = DatabaseManager.getConnection(); var preparedStatement = connection.prepareStatement("DELETE auth")) {
 
             preparedStatement.executeUpdate();
 
@@ -30,5 +71,31 @@ public class SQLAuthDAO implements AuthDAO {
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
 
+    }
+
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  auth (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              PRIMARY KEY (`authToken`)
+            )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            //throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 }
