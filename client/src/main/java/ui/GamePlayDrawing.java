ee4393f18f631393f14c7705d2fa9be2b6481d8b
nathan <nathanlunceford@gmail.com>
2024-06-03 14:38:05 -0600

    Moved starter code into correct formation; wrote functions that return 2-D arrays for white perspective and black perspective.

---
+++ client/src/main/java/ui/GamePlayDrawing.java
@@ -0,0 +1,113 @@
+package ui;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+//import java.util.Random;
+//import src.main.java.chess.ChessBoard;
+
+import static ui.EscapeSequences.*;
+
+public class GamePlayDrawing {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_CHARS = 3;
+    private static final int LINE_WIDTH_IN_CHARS = 1;
+    /**
+    private static final String WHITE_KING = " ♔ ";
+    private static final String WHITE_QUEEN = " ♕ ";
+    private static final String WHITE_BISHOP = " ♗ ";
+    private static final String WHITE_KNIGHT = " ♘ ";
+    private static final String WHITE_ROOK = " ♖ ";
+    private static final String WHITE_PAWN = " ♙ ";
+    private static final String BLACK_KING = " ♚ ";
+    private static final String BLACK_QUEEN = " ♛ ";
+    private static final String BLACK_BISHOP = " ♝ ";
+    private static final String BLACK_KNIGHT = " ♞ ";
+    private static final String BLACK_ROOK = " ♜ ";
+    private static final String BLACK_PAWN = " ♟ ";
+    private static final String EMPTY = " \u2003 ";
+    */
+    private static final String WHITE_KING = EscapeSequences.WHITE_KING;
+    private static final String WHITE_QUEEN = EscapeSequences.WHITE_QUEEN;
+    private static final String WHITE_BISHOP = EscapeSequences.WHITE_BISHOP;
+    private static final String WHITE_KNIGHT = EscapeSequences.WHITE_KNIGHT;
+    private static final String WHITE_ROOK = EscapeSequences.WHITE_ROOK;
+    private static final String WHITE_PAWN = EscapeSequences.WHITE_PAWN;
+    private static final String BLACK_KING = EscapeSequences.BLACK_KING;
+    private static final String BLACK_QUEEN = EscapeSequences.BLACK_QUEEN;
+    private static final String BLACK_BISHOP = EscapeSequences.BLACK_BISHOP;
+    private static final String BLACK_KNIGHT = EscapeSequences.BLACK_KNIGHT;
+    private static final String BLACK_ROOK = EscapeSequences.BLACK_ROOK;
+    private static final String BLACK_PAWN = EscapeSequences.BLACK_PAWN;
+    private static final String EMPTY = EscapeSequences.EMPTY;
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        //drawHeaders(out);
+
+        //drawTicTacToeBoard(out);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private String[][] whiteBoard(ChessBoard board) {
+        String[][] myBoard = new String[8][8];
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                myBoard[row][col] = thisPiece(board.getPiece(new ChessPosition(row + 1, col + 1)));
+            }
+        }
+        return myBoard;
+    }
+
+    private String[][] blackBoard(ChessBoard board) {
+        String[][] myBoard = new String[8][8];
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                myBoard[row][col] = thisPiece(board.getPiece(new ChessPosition(8 - row, 8 - col)));
+            }
+        }
+        return myBoard;
+    }
+
+    private String thisPiece(ChessPiece chessPiece) {
+        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            if (chessPiece.getPieceType() == ChessPiece.PieceType.KING)
+                return WHITE_KING;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.QUEEN)
+                return WHITE_QUEEN;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.BISHOP)
+                return WHITE_BISHOP;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.KNIGHT)
+                return WHITE_KNIGHT;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.ROOK)
+                return WHITE_ROOK;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.PAWN)
+                return WHITE_PAWN;
+        }
+        else if (chessPiece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+            if (chessPiece.getPieceType() == ChessPiece.PieceType.KING)
+                return BLACK_KING;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.QUEEN)
+                return BLACK_QUEEN;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.BISHOP)
+                return BLACK_BISHOP;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.KNIGHT)
+                return BLACK_KNIGHT;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.ROOK)
+                return BLACK_ROOK;
+            else if (chessPiece.getPieceType() == ChessPiece.PieceType.PAWN)
+                return BLACK_PAWN;
+        }
+        return EMPTY;
+    }
+}
